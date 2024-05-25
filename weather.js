4175a9eba3cbdb82047f2233e01d38803967f5e3
ToyLess78 <121128928+ToyLess78@users.noreply.github.com>
2024-05-25 22:52:03 +0300

    + implement saveToken

--- weather.js
+++ weather.js
@@ -1,11 +1,28 @@
 #!/usr/bin/env node
 import { getArgs } from './helpers/args.helper.js'
-import { printHelp } from "./services /log.service.js";
+import { printError, printHelp, printSuccess } from './services /log.service.js';
+import { saveKeyValue, TOKEN_DICTIONARY } from './services /storage.service.js';
+
+const saveToken = async (token) => {
+    if (!token.length) {
+        printError('Token not transferred');
+        return;
+    }
+    try {
+        await saveKeyValue(TOKEN_DICTIONARY.token, token);
+        printSuccess('Toke saved');
+    } catch (e) {
+        printError(e.message);
+    }
+}
 
 const initCLI = () => {
     const args = getArgs(process.argv);
     if (args.h) {
         return printHelp();
+    }
+    if (args.t) {
+        return saveToken(args.t);
     }
 };
 
